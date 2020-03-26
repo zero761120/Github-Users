f50f7abd6e9a16d4ad042160465b2a49cfece5ea
Louis.Wu <zero761120@gmail.com>
2020-03-26 10:20:16 +0800

    create default project

---
+++ app/src/main/java/com/ttchain/githubusers/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ttchain.githubusers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ttchain.githubusers.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
